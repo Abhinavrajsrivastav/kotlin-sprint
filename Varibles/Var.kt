c6509c64c14dbe79d413df310f72e813b0d4e03c
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-06-29 00:57:30 +0530

    29-06-2024-001

---
+++ Varibles/Var.kt
@@ -0,0 +1,7 @@
+
+fun main(args: Array<String>) {
+    val name = "Kotlin"
+    //name = "Java" // Error: Val cannot be reassigned
+    //name = 10 // Error: Type mismatch
+    println("Hello, $name!")
+}
